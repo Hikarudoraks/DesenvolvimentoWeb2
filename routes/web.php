5b1e7896c2a1e1bf46c9f1b85efd0ee4b5ca3011
Hikarudoraks <54286565+Hikarudoraks@users.noreply.github.com>
2020-09-03 09:33:06 -0300

    Atualização da aula 20

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,24 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TipoProdutoController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| This file is where you may define all of the routes that are handled
-| by your application. Just tell Laravel the URIs it should respond
-| to using a Closure or controller method. Build something great!
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('tipoproduto', "TipoProdutoController");
+// Route::get("tipoprodutos/create","TipoProdutoController@create")->name("tipoproduto.create");
+// Route::post("tipoprodutos","TipoProdutoController@store")->name("tipoproduto.store");
+
